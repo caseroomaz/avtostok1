9db4c752b3c153a5b1945641105356450a536b42
caseroomaz <uzeyirhuseynzade008@gmail.com>
2026-03-20 14:05:39 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Salam! Sayt isleyir. Demeli problem bazaya qosulma linkindedir."
+    # Bu bizə bazanın bağlı olub-olmadığını göstərəcək
+    url = os.environ.get('POSTGRES_URL', 'BAZA BAGLI DEYIL')
+    return f"Sayt isleyir! Baza veziyyeti: {url[:15]}..."
 
 if __name__ == "__main__":
     app.run()
